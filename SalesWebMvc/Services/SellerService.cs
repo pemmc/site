21345784ffd67f1ce2144fb6d9fdcc58b9cd9983
Paulo Eduardo Magaldi <pauloeduardomagaldi@hotmail.com>
2020-11-05 20:39:12 -0300

    Implementing simple searchsearch

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -48,7 +48,7 @@ namespace SalesWebMvc.Services
                 await _context.SaveChangesAsync();
             }
 
-            catch (DbUpdateException e)
+            catch (DbUpdateException)
             {
                 throw new IntegrityException("Não é possível deletar este vendedor, pois ele possui vendas!");
 
